d29cc1178a088b7b851207541c890a25d551f240
Alastair John Ludington <alastair.ludington@adelaide.edu.au>
2017-10-23 14:20:39 +1030

    Trawl al version
    Enjoy!

--- trawl_data/diversity_index.R
+++ trawl_data/diversity_index.R
@@ -7,6 +7,11 @@ library(tidyr)
 
 setwd("/Users/L033060262053/Documents/working_dir/activityPatterns/")
 
+# site <- c(1,1,1,1,1,10,11,11,11,12,13,13,13,14,14)
+# species <- LETTERS[1:15]
+# n <- c(4,5,11,5,1,4,12,4,3,4,7,2,3,3,2)
+# df <- data.frame(site,species,n)
+
 Dof <- read.csv("Dof_combined.csv", header = TRUE, sep = ",")# Diversity index between sites within EG
 
 # Brillouin index (Hb) function
@@ -24,5 +29,15 @@ ns <- Dof %>%
 head(ns)
 
 Hb(ns)
+
+## Al's solution
+df %>% 
+  split(f = .$site) %>%
+  lapply(function(x){
+    vec <- sum(x$n)
+    (lfactorial(vec) - sum(lfactorial(x$n)))/vec
+  }) %>%
+  bind_rows()
+  
 
 
